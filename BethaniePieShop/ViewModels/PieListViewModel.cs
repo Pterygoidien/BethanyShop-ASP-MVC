c9ef2859fa3ed98e18dd86a8a4b6cba874be0b12
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-12-26 19:03:47 +0100

    MVC| Add View Model for pies

---
+++ BethaniePieShop/ViewModels/PieListViewModel.cs
@@ -0,0 +1,15 @@
+using BethaniePieShop.Models;
+namespace BethaniePieShop.ViewModels;
+
+public class PieListViewModel
+{
+    public IEnumerable<Pie> Pies { get; set; }
+    public string? CurrentCategory { get; set; }
+
+    public PieListViewModel(IEnumerable<Pie> pies, string? currentCategory)
+    {
+        Pies = pies;
+        CurrentCategory = currentCategory;
+    }
+
+}
