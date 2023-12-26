d18de66215df188b29c1d891e3d82eb44cb718cc
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-12-26 21:11:13 +0100

    Model| Create DbContext (BethaniePieShopDbContext)

---
+++ BethaniePieShop/Models/BethaniePieShopDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BethaniePieShop.Models;
+
+public class BethaniePieShopDbContext : DbContext
+{
+    public BethaniePieShopDbContext(DbContextOptions<BethaniePieShopDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Pie> Pies { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+}
