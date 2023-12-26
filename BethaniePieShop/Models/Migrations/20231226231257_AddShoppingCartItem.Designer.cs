4ccee641ec036ba82b6f598171b17c42b0a897a0
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-12-27 00:15:06 +0100

    Model| Make migrations for Shopping Cart Item Model

--- BethaniePieShop/Models/Migrations/20231226231257_AddShoppingCartItem.Designer.cs
+++ BethaniePieShop/Models/Migrations/20231226231257_AddShoppingCartItem.Designer.cs
@@ -3,6 +3,7 @@ using BethaniePieShop.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BethaniePieShop.Models.Migrations
 {
     [DbContext(typeof(BethaniePieShopDbContext))]
-    partial class BethaniePieShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231226231257_AddShoppingCartItem")]
+    partial class AddShoppingCartItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
