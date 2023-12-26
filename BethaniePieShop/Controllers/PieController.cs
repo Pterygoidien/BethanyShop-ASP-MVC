5f7e4d073b873cba76ae0d1586d72b96a1af2e6d
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-12-26 18:49:10 +0100

    Controller| Add Pie Controller

---
+++ BethaniePieShop/Controllers/PieController.cs
@@ -0,0 +1,23 @@
+using BethaniePieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethaniePieShop.Controllers;
+
+public class PieController : Controller
+{
+    private readonly IPieRepository _pieRepository;
+    private readonly ICategoryRepository _categoryRepository;
+
+    public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+    {
+        _pieRepository = pieRepository;
+        _categoryRepository = categoryRepository;
+    }
+
+    public IActionResult List()
+    {
+        ViewBag.CurrentCategory = "Cheese cakes";
+        return View(_pieRepository.AllPies);
+    }
+
+}
