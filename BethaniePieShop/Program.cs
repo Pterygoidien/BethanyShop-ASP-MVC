9df046fd7468379133a132175f9eeb3a588825f2
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-12-26 21:12:56 +0100

    APP| Replace mockrepositories for the actual repositories in the DI container (MockPieRepository->PieRepository)

--- BethaniePieShop/Program.cs
+++ BethaniePieShop/Program.cs
@@ -1,12 +1,18 @@
 using BethaniePieShop.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<IPieRepository, MockPieRepository>();
-builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddScoped<IPieRepository, PieRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 builder.Services.AddControllersWithViews();
 
+// Register DBContext through dependency injection
+builder.Services.AddDbContext<BethaniePieShopDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]
+    ));
 
 var app = builder.Build();
 
